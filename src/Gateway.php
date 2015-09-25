29dbc5e8c7c69ce358a1fb6203e74d85079beaed
Luciano Jr <luciano@lucianojr.com.br>
2015-09-24 22:57:07 -0300

    Add new tests to Gateway class and implement some get/set methods parameters

--- src/Gateway.php
+++ src/Gateway.php
@@ -11,17 +11,66 @@ class Gateway extends AbstractGateway
     /**
      * Get gateway display name
      *
-     * This can be used by carts to get the display name for each gateway.
+     * This can be used by carts to get the display name gateway.
+     *
+     * @return string
      */
     public function getName()
     {
         return 'MoIP Payment';
     }
 
+    /**
+     * Get gateway display short name
+     *
+     * This can be used by developers to get the short display name gateway.
+     *
+     * @return string
+     */
     public function getShortName()
     {
         return 'MoIP';
     }
 
+
+    /**
+     * Set token authorization service
+     *
+     * @param string $token
+     */
+    public function setToken($token)
+    {
+        $this->setParameter('token', $token);
+    }
+
+    /**
+     * Get token authorization service
+     *
+     * @return string
+     */
+    public function getToken()
+    {
+        return $this->getParameter('token');
+    }
+
+    /**
+     * Set api key authentication service
+     *
+     * @param string $apiKey
+     */
+    public function setApiKey($apiKey)
+    {
+        $this->setParameter('apiKey', $apiKey);
+    }
+
+    /**
+     * Get api key authentication service
+     *
+     * @return string $apiKey
+     */
+    public function getApiKey()
+    {
+        return $this->getParameter('apiKey');
+    }
 
 }
--- tests/GatewayTest.php
+++ tests/GatewayTest.php
@@ -9,6 +9,8 @@ use Omnipay\Tests\GatewayTestCase;
 class GatewayTest extends GatewayTestCase
 {
 
+    protected $gateway;
+
     public function setUp()
     {
         parent::setUp();
@@ -23,6 +25,58 @@ class GatewayTest extends GatewayTestCase
 
     public function testShouldBeReturnGatewayName()
     {
-        $this->assertNotEquals("MoIP", $this->getName(), "The gateway name do not matter with the MoIP");
+        $this->assertEquals("MoIP Payment", $this->gateway->getName(), "The gateway name do not matter with the 'MoIP Payment'");
+    }
+
+    public function testShouldBeReturnGatewayShortName()
+    {
+        $this->assertEquals("MoIP", $this->gateway->getShortName(), "The gateway name do not matter with the 'MoIP'");
+    }
+
+    /**
+     * @dataProvider dataProvider
+     */
+    public function testIfTokenServiceIsSetup($token)
+    {
+
+        $this->gateway->setToken($token);
+        $this->assertEquals($token, $this->gateway->getToken(), "The token set is not equals same that passed");
+    }
+
+    /**
+     * @dataProvider dataProvider
+     */
+    public function testIfTokenServiceIsSetupIsNotTheSameWithWrongToken($token)
+    {
+
+        $this->gateway->setToken($token.'salt3@%$#');
+        $this->assertNotEquals($token, $this->gateway->getToken(), "The token set is not equals same that passed");
+    }
+
+    /**
+     * @dataProvider dataProvider
+     */
+    public function testIfApiKeyServiceIsSetup($apiKey)
+    {
+        $this->gateway->setApiKey($apiKey);
+        $this->assertEquals($apiKey, $this->gateway->getApiKey(), "The api key set is not the same that passed");
+    }
+
+    /**
+     * @dataProvider dataProvider
+     */
+    public function testIfApiKeyServiceIsSetupIsNotTheSameWithWrongApiKey($apiKey)
+    {
+        $this->gateway->setApiKey($apiKey.'salt3@%$#');
+        $this->assertNotEquals($apiKey, $this->gateway->getApiKey(), "The api key set is not the same that passed");
+    }
+
+    public function dataProvider()
+    {
+        return array(
+            array('ABABABABABABABABABABABABABAB'),
+            array('CBCBCBCBCBCBCBCBCBCBCBCBCBCB'),
+            array('ACACACACACACACACACACACACACAC'),
+        );
     }
 }
