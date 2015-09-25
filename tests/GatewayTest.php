fa00ce30077edaf841471f91988895a6a945239a
Luciano Jr <luciano@lucianojr.com.br>
2015-09-24 22:09:59 -0300

    Fix autoload

---
+++ tests/GatewayTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: luciano
+ * Date: 24/09/15
+ * Time: 21:23
+ */
+
+namespace Bavarianlabs\Omnipay\Test\Moip;
+
+
+class GatewayTest extends \PHPUnit_Framework_TestCase
+{
+
+}
